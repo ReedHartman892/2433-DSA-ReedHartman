34676c9b7290e98fefffad50c6f2aa5cbeb58e9f
Reed H. <texdirt2025@gmail.com>
2026-09-13 22:19:46 +0000

    program 1 progress

--- Program 1/cideogame.cpp
+++ Program 1/cideogame.cpp
@@ -3,7 +3,7 @@
 #include <iomanip> // text formatting
 #include <stdexcept> // error handling
 #include <fstream> // open input/output files
-#include <vector> // dynamic array
+#include <vector> // dynamic array option
 using namespace std;
 
 // Function prototypes
@@ -14,33 +14,33 @@ void debugOpenFiles(ifstream& infile, ofstream& outfile);
 // debug version of openFiles, makes testing quicker by removing the input prompts.
 
 int main() {
-    // file names
-    ifstream infile;
-    ofstream outfile;
-    debugOpenFiles(infile, outfile);
+    ifstream infile; // input file
+    ofstream outfile; // output file
+    //openFiles(infile, outfile); // open input/output files
+    debugOpenFiles(infile, outfile); // debug 'openFiles' option
+    vector<unsigned short> numbers; // vector (dynamic array)
     
-    vector<unsigned short> numbers;
-
-    unsigned short number;
     // while loop to insert the numbers from the data file into a vector (dynamic array)
+    unsigned short number;
     while(infile >> number) {
         numbers.push_back(number);
-        cout << number << ", ";
+        cout << number << " ";
     } cout << endl;
     
     // do-while loop iterate, 1D game of life
-    /*
+    bool go = false; // controls do-while iteration
     do {
         // bitwise operations to update state of each cell in the array, see rubric instructions for details
         // print out the data on each iteration, clean format
-    } while();
-    */
+        // prompt yes/no to continue simulation
+    } while(go == true);
+    
     // program end
     cout << "program ended\n";
     return 0;
 }
 
-void debugOpenFile (ifstream& infile, ofstream& outfile)
+void debugOpenFiles (ifstream& infile, ofstream& outfile)
 {
     infile.open("bits1.dat");
     outfile.open("output.txt");
