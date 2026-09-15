f2f02a7eb293d8224d778afa48f6d1347b14e4ca
Reed H. <texdirt2025@gmail.com>
2026-09-15 18:03:56 +0000

    program 1 main bitwise complete

--- Program 1/hartman_lifegame copy.cpp
+++ Program 1/hartman_lifegame copy.cpp
@@ -16,73 +16,84 @@ void closeFiles(ifstream& infile, ofstream& outfile);
    Recieves: 
    Returns:  */
 
-void debugOpenFiles(ifstream& infile, ofstream& outfile);
-// debug version of openFiles, makes testing quicker by removing the input prompts.
-
 vector<unsigned short> fillArray(ifstream& infile);
 // fills the array (or vector in this case) with the data values from the selected infile from openFiles
-
-short twoPower(short exp);
-// finds two to a power of exp, used in the masking portion of the program.
 
 int findSumOfArray(vector<unsigned short> array);
 // finds the sum of an array
 
+void generationStatement(vector<unsigned short> array, int genNumber);
+// print statement
+
 // Program Definitions
 #define ODD 1 // definition of odd
+#define EVEN 0 // definition of even
+#define MASK 1 // definition of mask
 
 int main() {
     ifstream infile; // input file
     ofstream outfile; // output file
     //openFiles(infile, outfile); // open input/output files
-    debugOpenFiles(infile, outfile); // debug 'openFiles' option
+    openFiles(infile, outfile); // debug 'openFiles' option
     vector<unsigned short> numbers = fillArray(infile); // vector (dynamic array)
     
+    // print statement
+    cout << endl;
+    cout << "Reed Hartman" << endl;
+    cout << "Program #1" << endl << endl;
+
     // do-while loop iterate, 1D game of life
     bool go = true; // controls do-while iteration
     int i = 0; // generation number
-    do {
-        for (int j = 1; j < numbers.size(); j++) // for loop to loop through array
-        {
-            short numi = numbers[j];
-            if (j == 1) // 1st element
-            {
-                numbers[j] = numi >> 1;
-            }
-            else if (j == numbers.size()-1) // last element
-            {
-                // toggles the i'th bit
-                numbers[j] = numi & ~(1 << i);
-            }
-            else // even/odd elements
-            {
-                if (j & ODD)
-                {
-                    
-                }
-                else // even numbers
-                {
-                    
-                }
-            }
-            cout << numbers[j] << " "; // print numbers
-        }
-        cout << endl;
-        i++; // increment generation
-        if (i >= 8) {go = false;} // failsafe
 
+    do {
+        if (i == 0) // first generation runs this code only
+        {
+            cout << "Generation 0: ";
+            for (int k = 0; k < numbers.size(); k++) // vector for loop to print numbers
+            {
+                cout << setw(4) << right << numbers[k];
+            }
+            cout << endl;
+        }
+        else // all subsequent generations run this code
+        {
+            cout << "Generation " << i << ": ";
+            for (int j = 0; j < numbers.size(); j++) // for loop to loop through array
+            {
+                unsigned short numi = numbers[j]; // temporary number value, "number at index"
+                if (j == 0) // 1st element
+                {
+                    numbers[j] = numi >> 1;
+                }
+                else if (j == numbers.size()-1) // last element
+                {
+                    // toggles the i'th bit
+                    numbers[j] = numi & ~(MASK << i);
+                }
+                else // even/odd elements
+                {
+                    if ((1 <= j <= numbers.size()-1) && (j & ODD)) // odd elements
+                    {
+                        numbers[j] = numi & numbers[j-1];
+                    }
+                    else // even elements
+                    {
+                        numbers[j] = numi ^ numbers[j+1];
+                    }
+                }
+                cout << setw(4) << right << numbers[j]; // print numbers
+            }
+            cout << endl;
+        }
+        i++; // increment generation number
+        if (i >= 9) {go = false;} // failsafe
     } while(go == true);
     closeFiles(infile, outfile);
 
     // program end
     cout << "program ended\n";
     return 0;
-}
-
-void debugOpenFiles (ifstream& infile, ofstream& outfile)
-{
-    infile.open("bits1.dat");
-    outfile.open("hartman_bits2.out");
 }
 
 void openFiles (ifstream& infile, ofstream& outfile)
@@ -108,28 +119,18 @@ void closeFiles (ifstream& infile, ofstream& outfile)
 
 vector<unsigned short> fillArray(ifstream& infile)
 {
+    
     vector<unsigned short> data; // dynamic array to be returned
     unsigned short num; // temporary value
-    
-    cout << "array: ";
-
+    cout << "Sample input: ";
     // while loop to insert the numbers from the data file into a vector (dynamic array)
     while(infile >> num) {
         data.push_back(num); // add next data value to the array
-        cout << num << " "; // debugging, remove later
+        cout << num << " ";
     }
+    data.erase(data.begin()); // remove first value, i dont know why this is needed but everything breaks and the program instruction always excludes the first bit.
     cout << endl; // debugging, remove later
     return data;
-}
-
-short twoPower(short exp)
-{
-    short x = 2;
-    short result = 1;
-    for (int i = 0; i < exp; ++i){
-        result *= x;
-    }
-    return result;
 }
 
 int findSumOfArray(vector<unsigned short> array)
